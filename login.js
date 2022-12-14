9e68b28826c09a5174ef48094fa9ffae6ff0c9e6
Display None <74074603+dr-kto@users.noreply.github.com>
2022-12-15 00:43:23 +0600

    login

---
+++ login.js
@@ -0,0 +1,22 @@
+function sendMail()
+{
+    var params = {
+        username: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value,
+    };
+
+    const serviceID = "service_kvnqmtt"
+    const templateID = "template_yso3mcx"
+
+    emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+        alert("Check your email "+document.getElementById("username").value);
+        document.getElementById("username").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+        console.log(res);
+    })
+    .catch((err) => console.log(err));
+}
