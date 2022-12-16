27956cf79d4d8006c71ded7fd41c8efd27dac369
dr-kto <200103136@stu.sdu.edu.kz>
2022-12-16 15:36:49 +0600

    j

--- login.js
+++ login.js
@@ -1,5 +1,4 @@
-function sendMail()
-{
+function sendMail() {
     var params = {
         username: document.getElementById("username").value,
         email: document.getElementById("email").value,
@@ -10,14 +9,17 @@ function sendMail()
     const templateID = "template_yso3mcx"
 
     emailjs
-    .send(serviceID, templateID, params)
-    .then((res) => {
-        alert("Check your email "+document.getElementById("username").value);
-        document.getElementById("username").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("password").value = "";
-        console.log(res);
-    })
-    .catch((err) => console.log(err));
-    window.location.href='./index.html'
+        .send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("username").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("password").value = "";
+            console.log(res);
+        })
+        .catch((err) => console.log(err));
+
+
+
+    window.location.href = './index.html'
+    alert("Check your email " + document.getElementById("username").value);
 }
